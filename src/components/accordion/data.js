d7c438eacd663bb944be201343b933d48424c979
osman-mahmud-antor <ontormahmud7877@gmail.com>
2024-10-10 13:31:21 +0600

    second commit since first one showing src redspec main does not match any

---
+++ src/components/accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      id  : '1',
+      question: "COMPUTER SCIENCE AND ENGINEERING",
+      answer:
+        "Most CSE programs require introductory mathematical knowledge, hence the first year of study is dominated by mathematical courses, primarily discrete mathematics, mathematical analysis, linear algebra, probability, and statistics, as well as the introduction to physics and electrical and electronic engineering.",
+    },
+    {
+      id  : '2',
+      question: "ELECTRONICS AND TELECOMMUNICATION ENGINEERING",
+      answer:
+        "Electronics and telecommunications engineering ( ETE) is a branch of electrical engineering that focuses on designing and developing electronic technology products. Electrical engineers work in many sectors, including healthcare, media and communications and software technology.",
+    },
+    {
+      id  : '3',
+      question: "MECHANICAL ENGINEERING",
+      answer:
+        "the field of mechanical engineering touches virtually every aspect of modern life, including the human body, a highly complex machine.",
+    },
+    {
+      id  : '4',
+      question: "CIVIL ENGINEERING",
+      answer:
+        "Civil engineering is a professional engineering discipline that deals with the design, construction, and maintenance of the physical and naturally built environment, including public works such as roads, bridges, canals, dams, airports, sewage systems, pipelines, structural components of buildings, and railways.",
+    },
+  ];
+
+  export default data;
